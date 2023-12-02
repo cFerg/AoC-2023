79dcb94d8ce9d040864e58ee120bf0cbcbc9cdf3
Elite <mrcalebf@gmail.com>
2023-12-01 23:15:28 -0500

    Update Day01.kt

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,40 @@
+import java.io.File
+
+val wordList = mutableListOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val part1 = mutableListOf<Int>()
+    val part2 = mutableListOf<Int>()
+    File("Day1.txt").forEachLine { line ->
+        var currFirst = line.first{it.isDigit()}
+        var currLast = line.last{it.isDigit()}
+
+        part1.add("$currFirst$currLast".toInt())
+
+        var firstIndex = line.indexOfFirst { it.isDigit() }
+        var lastIndex = line.indexOfLast { it.isDigit() }
+
+        for (i in wordList){
+            if (line.contains(i)){
+                val firstWord = line.indexOf(i)
+                val lastWord = line.lastIndexOf(i)
+
+                if (firstWord < firstIndex){
+                    firstIndex = firstWord
+                    currFirst = i.wordToNum().digitToChar()
+                }
+                if (lastWord > lastIndex){
+                    lastIndex = lastWord
+                    currLast = i.wordToNum().digitToChar()
+                }
+            }
+        }
+
+        part2.add("$currFirst$currLast".toInt())
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println("Answer to part 1: ${part1.sum()}")
+    println("Answer to part 2: ${part2.sum()}")
 }
+
+fun String.wordToNum() = wordList.indexOf(this) + 1
